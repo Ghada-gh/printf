2c7f13344c8692e64220a03df14706b043b7b0db
Ghada-gh <ghadaghmougui@yahoo.com>
2023-10-15 02:16:56 -0700

    new commit

--- 0_formatted_output.c
+++ 0_formatted_output.c
@@ -13,47 +13,7 @@
  * @format: Format string
  * Return: Number of characters printed  
  */
-/*
-int _printf(const char *format, ...)
-{
-  va_list args;
-  int i, count = 0;
-  char *str;
-
-  va_start(args, format);
-
-  for (i = 0; format[i] != '\0'; i++)
-    {
-      if (format[i] == '%')
-        {
-          switch (format[i + 1])
-            {
-            case 'c':
-              count += write(1, (void *) (intptr_t) va_arg(args, int), 1);
-              break;
-            case 's':
-              str = va_arg(args, char *);
-              count += write(1, str, strlen(str));
-              break;
-            case '%':
-              count += write(1, "%", 1);
-              break;
-            default:
-              count += write(1, "%", 1);
-              break;
-            }
-          i++;
-        }
-      else
-        {
-          count += write(1, &format[i], 1);
-        }
-    }
-  va_end(args);
-  return (count);
-}
-*/
- 
+/* 
 void handle_conversion(const char *format, va_list args, char *buffer)
 {
   char *temp = buffer;
@@ -141,9 +101,8 @@ int _printf(const char *format, ...)
   fwrite(buffer, sizeof(char), length, stdout);
   return (length);
 }
+*/
 
-
-/*
 int _printf(const char *format, ...)
 {
   int count = 0;
@@ -191,4 +150,3 @@ int _printf(const char *format, ...)
   va_end(args);
   return count;
 }
-*/
