994543341940ffcbe8fbc5d59a2d87d44a5a5b6e
Ghada-gh <ghadaghmougui@yahoo.com>
2023-10-15 05:36:10 -0700

    new commit

--- 0_formatted_output.c
+++ 0_formatted_output.c
@@ -13,95 +13,6 @@
  * @format: Format string
  * Return: Number of characters printed  
  */
-/* 
-void handle_conversion(const char *format, va_list args, char *buffer)
-{
-  char *temp = buffer;
-
-  while (*format != '\0')
-    {
-      if (*format == '%')
-        {
-	  format++;
-	  if (*format == 'c')
-            {
-	      char c = (char)va_arg(args, int);
-	      *temp++ = c;
-            }
-	  else if (*format == 's')
-            {
-	      char *s = va_arg(args, char *);
-	      strcpy(temp, s);
-	      temp += strlen(s);
-            }
-	  else if (*format == '%')
-            {
-	      *temp++ = '%';
-            }
-	  else if (*format == 'd' || *format == 'i')
-            {
-	      int d = va_arg(args, int);
-	      sprintf(temp, "%d", d);
-	      temp += strlen(temp);
-            }
-	  else if (*format == 'u')
-            {
-	      unsigned int u = va_arg(args, unsigned int);
-	      sprintf(temp, "%u", u);
-	      temp += strlen(temp);
-            }
-	  else if (*format == 'o')
-            {
-	      unsigned int o = va_arg(args, unsigned int);
-	      sprintf(temp, "%o", o);
-	      temp += strlen(temp);
-            }
-	  else if (*format == 'x' || *format == 'X')
-            {
-	      unsigned int x = va_arg(args, unsigned int);
-	      sprintf(temp, "%x", x);
-	      temp += strlen(temp);
-            }
-	  else if (*format == 'p')
-            {
-	      void *p = va_arg(args, void *);
-	      sprintf(temp, "%p", p);
-	      temp += strlen(temp);
-            }
-	  else if (*format == '\0')
-            {
-	      break;
-            }
-	  else
-            {
-	      *temp++ = '%';
-	      *temp++ = *format;
-            }
-        }
-      else
-        {
-	  *temp++ = *format;
-        }
-      format++;
-    }
-  *temp = '\0';
-}
-
-int _printf(const char *format, ...)
-{
-  va_list args;
-  char buffer[4096];
-  int length;
-
-  va_start(args, format);
-  handle_conversion(format, args, buffer);
-  va_end(args);
-
-  length = strlen(buffer);
-  fwrite(buffer, sizeof(char), length, stdout);
-  return (length);
-}
-*/
 
 int _printf(const char *format, ...)
 {
@@ -143,6 +54,18 @@ int _printf(const char *format, ...)
 	      count += len;
             }
 	  /* End of task 1 */
+
+	  /*Added section for task 2: handling 'b' specifier */
+	  else if (*format == 'b')
+            {
+	      unsigned int num = va_arg(args, unsigned int);
+	      char binary_str[32];
+	      int len = sprintf(binary_str, "%u", num);
+	      write(1, binary_str, len);
+	      count += len;
+            }
+	  /* End of Task 2 */
+	  
 	  else if (*format == '%')
             {
 	      write(1, "%", 1);
